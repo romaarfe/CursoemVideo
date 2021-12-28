0f6abbc173b6204618f038d1a35ff909cfeb329b
Rodrigo Fernandes <romaarfe@gmail.com>
2021-12-28 10:58:45 +0000

    exercício 2

---
+++ ex002.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+nome = input('Digite seu nome: ')
+print('\033[1;31mÉ um prazer te conhecer, {}! '.format(nome))
